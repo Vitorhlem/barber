8d0c5995af27290604138f83cc6b051033793d49
Vitor Hugo Lemes <vitorhugolemes6@gmail.com>
2026-05-18 18:57:25 -0300

    Update database connection string and close session

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://petvacina_user:iBlGL1DrQpJN4pnUVM7XllbwfUbscSPT@dpg-d7edglvaqgkc73fv9g6g-a/petvacina"
+SQLALCHEMY_DATABASE_URL = "postgresql://petvacina_11cn_user:7LdHQ3KlQUuRLfUxVravwTgRlvWSehOi@dpg-d85ohoti849s7384goag-a/petvacina_11cn"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
